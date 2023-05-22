42a3346350bae81257c4b866d638b55fc6037133
Franck Mairot <franck.mairot@gmail.com>
2023-05-22 14:18:18 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,29 +1,19 @@
-import streamlit as st
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
+import pandas as pd
+import streamlit as st
 import spacy
-from spacy import displacy
 from nltk.probability import FreqDist
 import openai
 
 # Configuration de l'API OpenAI
-openai.api_key = ""
+openai.api_key = "YOUR_OPENAI_API_KEY"
 
 # Demande des clés API OpenAI dans la sidebar
 st.sidebar.title("OpenAI API Keys")
 api_key_1 = st.sidebar.text_input("Enter OpenAI API Key 1:")
 api_key_2 = st.sidebar.text_input("Enter OpenAI API Key 2:")
 api_key_3 = st.sidebar.text_input("Enter OpenAI API Key 3:")
-
-
-# Vérification des clés API OpenAI
-openai_api_keys = [api_key_1, api_key_2, api_key_3]
-openai_api_keys = [key for key in openai_api_keys if key.strip()]
-if not openai_api_keys:
-    st.error("Please enter at least one OpenAI API Key.")
-else:
-    openai.api_key = openai_api_keys[0]
 
 nlp = spacy.load('fr_core_news_sm')
 
@@ -53,130 +43,71 @@ def scrape_article(url):
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Extract headings
-        headings = [tag.text for tag in soup.find_all(['h1', 'h2', 'h3'])]
-
-        # Extract meta description
+        headings = [f"{tag.name}: {tag.text}" for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5'])]
         meta_description_tag = soup.find('meta', attrs={'name':'description'})
         meta_description = meta_description_tag['content'] if meta_description_tag else ""
-
-        # Extract main body content
         body_content = soup.find('body').text
-
-        # Count words in the main body content
         word_count = len(body_content.split())
+        semantic_field = analyze_text(body_content)
+        named_entity = get_named_entities(' '.join(headings))
 
-        return headings, word_count, meta_description, analyze_text(body_content), get_named_entities(' '.join(headings))
-
+        return headings, word_count, meta_description, semantic_field, named_entity
     except Exception as e:
-        print(f"Error scraping article: {e}")
+        print(f"An error occurred while trying to scrape the article at {url}. Error: {e}")
         return [], 0, "", "", ""
 
-    def scrape_google(query):
-        api_key = '8e87e954-6b75-4888-bd6c-86868540beeb'
-        url = f'https://api.spaceserp.com/google/search?apiKey={api_key}&q={query}&domain=google.fr&gl=fr&hl=fr&resultFormat=json&resultBlocks=organic_results%2Canswer_box%2Cpeople_also_ask%2Crelated_searches%2Cads_results'
-        response = requests.get(url).json()
-        results = []
-        all_titles = []
-        all_headings = []
-        word_counts = []
-        semantic_fields = []
-        named_entities = []
-        serp_descriptions = []
-        meta_descriptions = []
-        if 'organic_results' in response:
-            for result in response['organic_results']:
-                title = result['title']
-                url = result['link']
-                serp_description = result['description']
-                headings, word_count, meta_description, semantic_field, named_entity = scrape_article(url)
-                all_titles.append(title)
-                all_headings.append(' '.join(headings))
-                word_counts.append(word_count)
-                semantic_fields.append(semantic_field)
-                named_entities.append(named_entity)
-                serp_descriptions.append(serp_description)
-                meta_descriptions.append(meta_description)
-                results.append((title, url, ' '.join(headings), word_count, '', serp_description, meta_description, semantic_field, named_entity))
-        if 'people_also_ask' in response:
-            people_also_ask = [item['question'] for item in response['people_also_ask']]
-            all_questions = ' '.join(people_also_ask)
-        else:
-            all_questions = ''
-        if not results:
-            return pd.DataFrame(columns=['Title', 'URL', 'Headings', 'Word Count', 'People Also Ask', 'SERP Description', 'Site Meta Description', 'Semantic Field', 'Named Entities'])
-        df = pd.DataFrame(results, columns=['Title', 'URL', 'Headings', 'Word Count', 'People Also Ask', 'SERP Description', 'Site Meta Description', 'Semantic Field', 'Named Entities'])
-        df = df.rename(index={df.index[-1]: 'Résumé'})
-        df['Semantic Field'] = df['Semantic Field'].str.replace(' ', '  ')  # Ajout d'espaces entre deux expressions
-        df.at['Résumé', 'Title'] = ' '.join(all_titles[:10])  # Forcer le résumé à la 11e ligne
-        df.at['Résumé', 'URL'] = ''
-        df.at['Résumé', 'Headings'] = ' '.join(all_headings[:10])
-        df.at['Résumé', 'Word Count'] = pd.Series(word_counts).median()
-        df.at['Résumé', 'People Also Ask'] = all_questions
-        df.at['Résumé', 'SERP Description'] = ' '.join(serp_descriptions[:10])
-        df.at['Résumé', 'Site Meta Description'] = ' '.join(meta_descriptions[:10])
-        df.at['Résumé', 'Semantic Field'] = ' '.join(semantic_fields[:10])
-        df.at['Résumé', 'Named Entities'] = ' '.join(named_entities[:10])
+def get_openai_proposal(text, model="text-davinci-002"):
+    try:
+        response = openai.Completion.create(
+            engine=model,
+            prompt=text,
+            temperature=0.5,
+            max_tokens=60
+        )
+        return response.choices[0].text.strip()
+    except Exception as e:
+        print(f"An error occurred while trying to get OpenAI proposal: {e}")
+        return ""
 
-        # Stockage des contenus de chaque colonne dans des variables
-        title_summary = df.at['Résumé', 'Title']
-        headings_summary = df.at['Résumé', 'Headings']
-        word_count_summary = df.at['Résumé', 'Word Count']
-        people_also_ask_summary = df.at['Résumé', 'People Also Ask']
-        serp_description_summary = df.at['Résumé', 'SERP Description']
-        meta_description_summary = df.at['Résumé', 'Site Meta Description']
-        semantic_field_summary = df.at['Résumé', 'Semantic Field']
-        named_entities_summary = df.at['Résumé', 'Named Entities']
+def scrape_google(query):
+    api_key = '8e87e954-6b75-4888-bd6c-86868540beeb'
+    url = f'https://api.spaceserp.com/google/search?apiKey={api_key}&q={query}&domain=google.fr&gl=fr&hl=fr&resultFormat=json&resultBlocks=organic_results%2Canswer_box%2Cpeople_also_ask%2Crelated_searches%2Cads_results'
+    response = requests.get(url).json()
+    results = []
 
-        return df, title_summary, headings_summary, word_count_summary, people_also_ask_summary, serp_description_summary, meta_description_summary, semantic_field_summary, named_entities_summary
+    if 'organic_results' in response:
+        for result in response['organic_results']:
+            title = result.get('title', '')
+            url = result.get('link', '')
+            serp_description = result.get('description', '')
+            headings, word_count, meta_description, semantic_field, named_entity = scrape_article(url)
+
+            plan_proposal = get_openai_proposal(f"Plan for the article on {title}:")
+            meta_description_proposal = get_openai_proposal(f"Meta description for the article on {title}:")
+            title_proposal = get_openai_proposal(f"Title for the article on {title}:")
+
+            results.append((title, url, ' '.join(headings), word_count, '', serp_description, meta_description, semantic_field, named_entity, plan_proposal, meta_description_proposal, title_proposal))
+
+    if 'people_also_ask' in response:
+        people_also_ask = [item['question'] for item in response['people_also_ask']]
+        all_questions = ' '.join(people_also_ask)
+    else:
+        all_questions = ''
+
+    if not results:
+        return pd.DataFrame(columns=['Title', 'URL', 'Headings', 'Word Count', 'People Also Ask', 'SERP Description', 'Site Meta Description', 'Semantic Field', 'Named Entities', 'OpenAI Plan Proposal', 'OpenAI Meta Description Proposal', 'OpenAI Title Proposal'])
+
+    df = pd.DataFrame(results, columns=['Title', 'URL', 'Headings', 'Word Count', 'People Also Ask', 'SERP Description', 'Site Meta Description', 'Semantic Field', 'Named Entities', 'OpenAI Plan Proposal', 'OpenAI Meta Description Proposal', 'OpenAI Title Proposal'])
+    return df
 
 st.title("Google Scraper and Article Analyzer")
-query = st.text_input("Enter search queries (separated by commas):", key="input1")
+query = st.text_input("Enter search queries (separated by commas):")
 
-if st.button("Scrape Google", key='button1'):
-    # Initialisation des variables de résumé
-    title_summary = ""
-    headings_summary = ""
-    word_count_summary = ""
-    people_also_ask_summary = ""
-    serp_description_summary = ""
-    meta_description_summary = ""
-    semantic_field_summary = ""
-    named_entities_summary = ""
-
+if st.button("Scrape Google"):
     queries = [q.strip() for q in query.split(',')]
     for q in queries:
-        df, title_summary, headings_summary, word_count_summary, people_also_ask_summary, serp_description_summary, meta_description_summary, semantic_field_summary, named_entities_summary = scrape_google(q)
+        df = scrape_google(q)
         st.write(f"Results for {q}:")
         st.write(df)
-
-def generate_openai_prompt(keyword, headings_summary, word_count_summary):
-    messages = [
-        {"role": "system", "content": "You are a proficient SEO expert in France. Generate a detailed brief for a text about the provided keyword."},
-        {"role": "user", "content": f"Here are some good briefing tips for 2023: {headings_summary}. Based on these, can you propose a complete brief for a text about {keyword}, which should be about {word_count_summary} words long, including section titles, subsection titles, and word count for each section? Try to incorporate tables, images, lists, internal links, buttons, videos, etc. as needed."}
-    ]
-    response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
-    return response.choices[0].message.content
-
-def generate_meta_description_prompt(keyword, headings_summary, word_count_summary):
-    messages = [
-        {"role": "system", "content": "You are a proficient SEO expert in France. Create a meta description for a text about the provided keyword."},
-        {"role": "user", "content": f"Here are some good briefing tips for 2023: {headings_summary}. Based on these, can you propose a meta description for a text about {keyword}, which should be about {word_count_summary} words long?"}
-    ]
-    response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
-    return response.choices[0].message.content
-
-if st.button("Scrape Google", key='button2'):
-    queries = [q.strip() for q in query.split(',')]
-    for q in queries:
-        df, title_summary, headings_summary, word_count_summary, people_also_ask_summary, serp_description_summary, meta_description_summary, semantic_field_summary, named_entities_summary = scrape_google(q)
-        df['Plan'] = df.apply(lambda row: generate_openai_prompt(q, headings_summary, word_count_summary), axis=1)
-        df['Proposition Meta Description'] = df.apply(lambda row: generate_meta_description_prompt(q, headings_summary, word_count_summary), axis=1)
-        st.write(f"Results for {q}:")
-        st.write(df)
-
-
-
-
-
-
+        csv = df.to_csv(index=False)
+        st.download_button(label="Download CSV", data=csv, file_name="scraping_results.csv", mime="text/csv")
