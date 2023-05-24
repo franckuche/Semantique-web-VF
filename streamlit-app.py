a4c20693428b2e52b40c965d10d5a82c81941d41
Franck Mairot <franck.mairot@gmail.com>
2023-05-24 11:46:07 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -16,7 +16,6 @@ openai_api_key = st.sidebar.text_input("Enter OpenAI API Key:")
 # Configuration de l'API OpenAI
 openai.api_key = openai_api_key
 
-
 # Vérification de la clé API OpenAI
 if not openai_api_key.strip():
     st.error("Please enter an OpenAI API Key.")
@@ -50,21 +49,11 @@ def scrape_article(url):
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        # Extract headings with their respective tags
         headings = [f"{tag.name}: {tag.text}" for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5'])]
-
-        # Extract meta description
         meta_description_tag = soup.find('meta', attrs={'name':'description'})
         meta_description = meta_description_tag['content'] if meta_description_tag else ""
-
-        # Extract main body content
         body_content = soup.find('body').text
-
-        # Count words in the main body content
         word_count = len(body_content.split())
-
-        # Analyze semantic field and named entities
         semantic_field = analyze_text(body_content)
         named_entity = get_named_entities(' '.join(headings))
 
@@ -95,76 +84,40 @@ def generate_openai_proposals(keyword, semantic_field, headings):
 
 def scrape_google(query):
     api_key = '8e87e954-6b75-4888-bd6c-86868540beeb'
-    url = f'https://api.spaceserp.com/google/search?apiKey={api_key}&q={query}&domain=google.fr&gl=fr&hl=fr&resultFormat=json&resultBlocks=organic_results%2Canswer_box%2Cpeople_also_ask%2Crelated_searches%2Cads_results'
-    response = requests.get(url).json()
+    url = f'https://api.spaceserp.com/google/search?apiKey={api_key}&q={query}&device=desktop'
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
     results = []
-    all_titles = []
-    all_headings = []
-    word_counts = []
-    semantic_fields = []
-    named_entities = []
-    serp_descriptions = []
-    meta_descriptions = []
-    openai_proposals = []
-    if 'organic_results' in response:
-        for result in response['organic_results']:
-            title = result.get('title', '')
-            url = result.get('link', '')
-            serp_description = result.get('description', '')
+
+    for item in data['organicResults']:
+        if 'title' in item and 'url' in item:
+            title = item['title']
+            url = item['url']
             headings, word_count, meta_description, semantic_field, named_entity = scrape_article(url)
-            all_titles.append(title)
-            all_headings.append(' '.join(headings))
-            word_counts.append(word_count)
-            semantic_fields.append(semantic_field)
-            named_entities.append(named_entity)
-            serp_descriptions.append(serp_description)
-            meta_descriptions.append(meta_description)
-            results.append((title, url, ' '.join(headings), word_count, '', serp_description, meta_description, semantic_field, named_entity))
-
-            # Generate OpenAI proposals
-            openai_proposal = generate_openai_proposals(title, df.at['Résumé', 'Semantic Field'], df.at['Résumé', 'Headings'])
-            openai_proposals.append(openai_proposal)
-
-    if 'people_also_ask' in response:
-        people_also_ask = [item['question'] for item in response['people_also_ask']]
-        all_questions = ' '.join(people_also_ask)
-    else:
-        all_questions = ''
-
-    if not results:
-        return pd.DataFrame(columns=['Title', 'URL', 'Headings', 'Word Count', 'People Also Ask', 'SERP Description', 'Site Meta Description', 'Semantic Field', 'Named Entities']), pd.DataFrame(columns=['Keyword', 'Volume', 'Plan proposé'])
+            result = [title, url, headings, word_count, item.get('peopleAlsoAsk', ""), item.get('snippet', ""), meta_description, semantic_field, named_entity]
+            results.append(result)
 
     df = pd.DataFrame(results, columns=['Title', 'URL', 'Headings', 'Word Count', 'People Also Ask', 'SERP Description', 'Site Meta Description', 'Semantic Field', 'Named Entities'])
-    df = df.rename(index={df.index[-1]: 'Résumé'})
-    df['Semantic Field'] = df['Semantic Field'].str.replace(' ', '  ')  # Ajout d'espaces entre deux expressions
-    df.at['Résumé', 'Title'] = ' '.join(all_titles[:10])  # Forcer le résumé à la 11e ligne
-    df.at['Résumé', 'URL'] = ''
-    df.at['Résumé', 'Headings'] = ' '.join(all_headings[:10])
-    df.at['Résumé', 'Word Count'] = pd.Series(word_counts).median()
-    df.at['Résumé', 'People Also Ask'] = all_questions
-    df.at['Résumé', 'SERP Description'] = ' '.join(serp_descriptions[:10])
-    df.at['Résumé', 'Site Meta Description'] = ' '.join(meta_descriptions[:10])
-    df.at['Résumé', 'Semantic Field'] = ' '.join(semantic_fields[:10])
-    df.at['Résumé', 'Named Entities'] = ' '.join(named_entities[:10])
 
-    openai_df = pd.DataFrame(columns=['Keyword', 'Volume', 'Plan proposé'])
-    openai_df['Keyword'] = df['Title']
-    openai_df['Volume'] = ''
-    openai_df['Plan proposé'] = openai_proposals
+    openai_proposals = []
+    for title in df['Title']:
+        openai_proposal = generate_openai_proposals(title, df.at['Résumé', 'Semantic Field'], df.at['Résumé', 'Headings'])
+        openai_proposals.append(openai_proposal)
 
-    return df, openai_df
+    return df, pd.DataFrame(openai_proposals, columns=['OpenAI Proposal'])
 
 st.title("Google Scraper and Article Analyzer")
-query = st.text_input("Entrez vos requêtes de recherche (séparées par des virgules) :")
+query = st.text_input("Enter search queries (separated by commas):")
 
 if st.button("Scrape Google"):
     queries = [q.strip() for q in query.split(',')]
     for q in queries:
         google_df, openai_df = scrape_google(q)
-        st.write(f"Résultats pour {q} :")
-        st.write("Résultats Google :")
-        st.dataframe(google_df)
-        st.write("Résultats OpenAI :")
-        st.dataframe(openai_df)
+        st.write(f"Results for {q}:")
+        st.write("Google Results:")
+        st.write(google_df)
+        st.write("OpenAI Results:")
+        st.write(openai_df)
         csv = google_df.to_csv(index=False)
-        st.download_button(label="Télécharger le CSV", data=csv, file_name="scraping_results.csv", mime="text/csv")
+        st.download_button(label="Download CSV", data=csv, file_name="scraping_results.csv", mime="text/csv")
